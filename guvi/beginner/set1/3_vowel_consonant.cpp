2b4c6735900c4a8c489ea6b0bcbe9e01d642fefe
sk <code4all66@gmail.com>
2018-08-15 22:00:10 +0530

    Added to exclude invalid characters

--- guvi/beginner/set1/3_vowel_consonant.cpp
+++ guvi/beginner/set1/3_vowel_consonant.cpp
@@ -8,10 +8,12 @@ int main(void)
 	char c = 0;
 	cin >> c;
 	
-	if(c == 'a' | c == 'e' | c == 'i' | c == 'o' | c == 'u')
+	if(c == 'a' | c == 'e' | c == 'i' | c == 'o' | c == 'u' | c == 'A' | c == 'E' | c == 'I' | c == 'O' | c == 'U')
 		cout << "Vowel";
-	else
+	else if(int(c) >= 65 && int(c) <= 90 || int(c) >= 97 && int(c) <= 122 )
 		cout << "Consonant";
+	else
+		cout << "Invalid";
 	
 	return(0); 
 }
